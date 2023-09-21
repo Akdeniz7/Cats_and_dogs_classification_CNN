3e15a8bbcc9e5e109b55b27ec0fe998def185856
Ahmet Sezer <ahmetsezerakdeniz19@gmail.com>
2023-09-21 18:35:00 +0300

    added pictures

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ import numpy as np
 from PIL import Image
 from tensorflow.keras.preprocessing import image
 
-test_image = image.load_img('dog.jpg', target_size=(150, 150))
+test_image = image.load_img('cat.jpg', target_size=(150, 150))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
 result = model.predict(test_image)
